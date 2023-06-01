f15952b1eba55527ac50501eee5104dd6d3af4d2
gowth6m <gowtham4n@gmail.com>
2023-06-01 18:13:45 +0100

    Initial project setup

---
+++ src/app/head.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Head() {
+  const metadata = {
+    title: "NightBloom",
+    description:
+      "NightBloom is a tool that helps you find creative, unique, and AI-generated artworks.",
+    url: "https://nightbloom.ai",
+  };
+
+  return (
+    <head>
+      <title>{metadata.title}</title>
+      <meta name="description" content={metadata.description} />
+      <meta property="og:title" content={metadata.title} />
+      <meta property="og:description" content={metadata.description} />
+      <meta property="og:url" content={metadata.url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="/logo/logo.png" />
+      <meta name="twitter:title" content={metadata.title} />
+      <meta name="twitter:description" content={metadata.description} />
+      <meta name="twitter:image" content="/logo/logo.png" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="logo/logo.svg" />
+      <link rel="icon" href="logo/logo.svg" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href="logo/logo.png" />
+      <meta name="theme-color" content="#DFD5EC" />
+    </head>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="container mx-auto ">
+      <div className="w-full bg-slate-100 h-full">
+        NightBloom
+      </div>
+    </main>
+  );
+}
