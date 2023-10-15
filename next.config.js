7e2a5070a07b3e5a85826836101a2e5ae1b15af5
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2023-10-15 16:03:06 +0100

    Try fix style guide routing error

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
     output: 'export',
     trailingSlash: true,
 
-    // async rewrites() {
-    //     return [
-    //         {
-    //             source: 'api/google/oauth_return',
-    //             destination: '/sign-in/googleLogin'
-    //         }
-    //     ]
-    // }
 }
 
 module.exports = nextConfig
