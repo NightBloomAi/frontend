3c8d85f68ca4dfadc1fa02101838377c12eab44a
gowth6m <gowtham4n@gmail.com>
2023-10-24 14:20:51 +0100

    Add query id for style guide popup

--- src/components/style-guide/styleSearchResults.tsx
+++ src/components/style-guide/styleSearchResults.tsx
@@ -4,32 +4,39 @@ import LoadingSkeleton from "../misc/loadingSkeleton";
 import ErrorMsg from "../misc/error";
 import InfiniteScroll from "react-infinite-scroll-component";
 import StyleGalleryResults from "./styleGalleryResults";
+import { ReadonlyURLSearchParams } from "next/navigation";
 
 export default function StyleSearchResults({
-  data,
-  loading,
-  error,
-  category,
-  fetchMoreData,
+    data,
+    loading,
+    error,
+    category,
+    fetchMoreData,
+    params,
 }: {
-  data: Hit[] | undefined;
-  loading: boolean;
-  error: any;
-  category: string;
-  fetchMoreData: () => void;
+    data: Hit[] | undefined;
+    loading: boolean;
+    error: any;
+    category: string;
+    fetchMoreData: () => void;
+    params: ReadonlyURLSearchParams;
 }): JSX.Element {
-  if (loading) return <LoadingSkeleton />;
-  if (error || !data) return <ErrorMsg error={error} />;
-  return (
-    <InfiniteScroll
-      dataLength={data.length}
-      next={fetchMoreData}
-      hasMore={true}
-      loader={<></>}
-      endMessage={<p>No more data to load.</p>}
-      style={{ height: "auto", overflow: "visible" }}
-    >
-      <StyleGalleryResults data={data} category={category} />
-    </InfiniteScroll>
-  );
+    if (loading) return <LoadingSkeleton />;
+    if (error || !data) return <ErrorMsg error={error} />;
+    return (
+        <InfiniteScroll
+            dataLength={data.length}
+            next={fetchMoreData}
+            hasMore={true}
+            loader={<></>}
+            endMessage={<p>No more data to load.</p>}
+            style={{ height: "auto", overflow: "visible" }}
+        >
+            <StyleGalleryResults
+                data={data}
+                category={category}
+                params={params}
+            />
+        </InfiniteScroll>
+    );
 }
