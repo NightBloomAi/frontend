dd3195c4f82cab5f01670189d4fc62a2bef00ae2
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2023-10-31 04:17:00 +0000

    Fix search for no results returned

--- src/hooks/useSearch.tsx
+++ src/hooks/useSearch.tsx
@@ -3,86 +3,88 @@ import { Hit } from "@/types/searchRes.type";
 import { useReducer, useRef, useEffect } from "react";
 
 type State = {
-    data: Hit[] | undefined;
-    loading: boolean;
-    error: string | null;
-    moreData: boolean;
+  data: Hit[] | undefined;
+  loading: boolean;
+  error: string | null;
+  moreData: boolean;
 };
 
 type Action =
-    | { type: "LOAD"; payload: Hit[] }
-    | { type: "ERROR"; payload: string }
-    | {type: "FINISHEDDATA"}
-    | { type: "LOADING" };
+  | { type: "LOAD"; payload: Hit[] }
+  | { type: "ERROR"; payload: string }
+  | { type: "FINISHEDDATA" }
+  | { type: "LOADING" };
 
 function reducer(state: State, action: Action): State {
-    switch (action.type) {
-        case "LOAD":
-            return { ...state, loading: false, data: action.payload };
-        case "ERROR":
-            return { ...state, loading: false, error: action.payload };
-        case "LOADING":
-            return { ...state, loading: true };
-        case "FINISHEDDATA":
-            return {...state, loading: false, moreData: false}
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "LOAD":
+      return { ...state, loading: false, data: action.payload };
+    case "ERROR":
+      return { ...state, loading: false, error: action.payload };
+    case "LOADING":
+      return { ...state, loading: true };
+    case "FINISHEDDATA":
+      return { ...state, loading: false, moreData: false };
+    default:
+      return state;
+  }
 }
 
 const useSearch = (initialPage: number, search: string, category: string) => {
-    const [state, dispatch] = useReducer(reducer, {
-        data: undefined,
-        loading: true,
-        error: null,
-        moreData: true,
-    });
+  const [state, dispatch] = useReducer(reducer, {
+    data: undefined,
+    loading: true,
+    error: null,
+    moreData: true,
+  });
 
-    const { data, loading, error, moreData } = state;
-    const pageRef = useRef(initialPage);
+  const { data, loading, error, moreData } = state;
+  const pageRef = useRef(initialPage);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            dispatch({ type: "LOADING" });
-            try {
-                const res = await Endpoints.search({
-                    page: pageRef.current,
-                    query: search,
-                    category,
-                });
-                    dispatch({ type: "LOAD", payload: res.hits });
-
-            } catch (error: any) {
-                dispatch({ type: "ERROR", payload: error.message });
-            }
-        };
-        fetchData();
-    }, [search, category]);
-
-    const fetchMoreData = async () => {
-        pageRef.current++;
-        try {
-            const res = await Endpoints.search({
-                page: pageRef.current,
-                query: search,
-                category,
-            });
-            if (res.hits.length=== 0) {
-                dispatch({type:"FINISHEDDATA"})
-            }
-            if (data) {
-                dispatch({ type: "LOAD", payload: data.concat(res.hits) });
-            }
-        } catch (error: any) {
-            dispatch({ type: "ERROR", payload: error.message });
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "LOADING" });
+      try {
+        const res = await Endpoints.search({
+          page: pageRef.current,
+          query: search,
+          category,
+        });
+        if (res.hits.length === 0) {
+          dispatch({ type: "FINISHEDDATA" });
         }
+        dispatch({ type: "LOAD", payload: res.hits });
+      } catch (error: any) {
+        dispatch({ type: "ERROR", payload: error.message });
+      }
     };
+    fetchData();
+  }, [search, category]);
 
-    const resetPage = () => {
-        pageRef.current = 1;
-    };
+  const fetchMoreData = async () => {
+    pageRef.current++;
+    try {
+      const res = await Endpoints.search({
+        page: pageRef.current,
+        query: search,
+        category,
+      });
+      if (res.hits.length === 0) {
+        dispatch({ type: "FINISHEDDATA" });
+      }
+      if (data) {
+        dispatch({ type: "LOAD", payload: data.concat(res.hits) });
+      }
+    } catch (error: any) {
+      dispatch({ type: "ERROR", payload: error.message });
+    }
+  };
 
-    return { moreData, data, loading, error, fetchMoreData, resetPage };
+  const resetPage = () => {
+    pageRef.current = 1;
+  };
+
+  return { moreData, data, loading, error, fetchMoreData, resetPage };
 };
 
 export default useSearch;
