354c6c111435062f7799bb20f37834a6979ba636
gowth6m <gowtham4n@gmail.com>
2023-12-04 01:56:50 +0000

    Add Google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,20 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
     return (
         <Html lang="en">
             <Head />
+            <Script id="google-analytics" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-SKQVC2JL0B`}>
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-SKQVC2JL0B', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+            </Script>
             <body>
                 <Main />
                 <NextScript />
